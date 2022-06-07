3b6a15afd203b0a88283fbb3d840922af44f34e2
Darin McGlauflin <christian.mcglauflin@us.af.mil>
2022-06-06 21:10:30 -0500

    Working card flipper

--- src/App.js
+++ src/App.js
@@ -2,26 +2,57 @@ import './App.css';
 import Footer from './Footer';
 import Cards from './Cards';
 import React, {useState} from 'react';
-import apple from "./Images/fruitsandvegetables_Apple.png";
-import banana from "./Images/fruitsandvegetables_Banana.png";
-import beetroot from "./Images/fruitsandvegetables_Beetroot.png";
-import fruitBowl from "./Images/fruitsandvegetables_Bowl_of_fruit.png";
-import boxProduct from "./Images/fruitsandvegetables_Box_of_products.png";
 
-const cardType = [
-  apple,
-  banana,
-  beetroot,
-  fruitBowl,
-  boxProduct
+window.flipCount = 0;
+
+const cardNum = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
 ];
 
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+// Used like so
+shuffle(cardNum);
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p> This is my Card Matching App </p>
-        <Cards />
+        <p> Number of flips is: {window.flipCount} </p>
+        <div className='cardGrid'>
+          <Cards fruit = {cardNum[0]}/>
+          <Cards fruit = {cardNum[1]}/>
+          <Cards fruit = {cardNum[2]}/>
+          <Cards fruit = {cardNum[3]}/>
+          <Cards fruit = {cardNum[4]}/>
+          <Cards fruit = {cardNum[5]}/>
+          <Cards fruit = {cardNum[6]}/>
+          <Cards fruit = {cardNum[7]}/>
+          <Cards fruit = {cardNum[8]}/>
+          <Cards fruit = {cardNum[9]}/>
+          <Cards fruit = {cardNum[10]}/>
+          <Cards fruit = {cardNum[11]}/>
+          <Cards fruit = {cardNum[12]}/>
+          <Cards fruit = {cardNum[13]}/>
+          <Cards fruit = {cardNum[14]}/>
+          <Cards fruit = {cardNum[15]}/>
+        </div>
       </header>
       <Footer />
     </div>
--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,32 +1,100 @@
 import React from "react";
-
-import apple from "./Images/fruitsandvegetables_Apple.png";
-//import banana from "./Images/fruitsandvegetables_Banana.png";
 import front from "./Images/front.jpg";
-import empty from "./Images/empty.png";
+import apple from "./Images/fruitsandvegetables_Apple.png";
+import banana from "./Images/fruitsandvegetables_Banana.png";
+import beetroot from "./Images/fruitsandvegetables_Beetroot.png";
+import fruitBowl from "./Images/fruitsandvegetables_Bowl_of_fruit.png";
+import boxProduct from "./Images/fruitsandvegetables_Box_of_products.png";
+import carrot from "./Images/fruitsandvegetables_Carrot.png";
+import cherry from "./Images/fruitsandvegetables_Cherry.png";
+import corn from "./Images/fruitsandvegetables_Corn.png";
 
-const Cards = () => {
+const cardType = [
+  apple, 
+  apple,
+  banana,
+  banana,
+  beetroot,
+  beetroot,
+  fruitBowl,
+  fruitBowl,
+  boxProduct,
+  boxProduct,
+  carrot,
+  carrot,
+  cherry,
+  cherry,
+  corn,
+  corn
+];
+
+let numFlips = 0;
+let firstcard = -1;
+let seccard = -1;
+
+const Cards = (props) => {
+  const {fruit} = props;
   const [cardState, setCardState] = React.useState(0);
-  var card = document.querySelector('.flipper');
+  var card = document.querySelector('.flipper' + fruit);
   const handleFlip = () => {
+    if (card === null) {
+      card = document.querySelector('.flipper' + fruit);
+    }
+    numFlips = numFlips + 1;
+    console.log('Number of flips = ' + numFlips);
+
+    if (numFlips === 1) {
+      firstcard = fruit;
+      card.classList.toggle('is-flipped'+firstcard);
+      window.flipCount = window.flipCount + 1;
+    }
+    else if (numFlips === 2) {
+      seccard = fruit;
+      card.classList.toggle('is-flipped'+seccard);
+      window.flipCount = window.flipCount + 1;
+      if ((firstcard%2 === 0 && firstcard !== seccard- 1) || (firstcard%2 === 1 && firstcard !== seccard + 1)) {
+
+        setTimeout(()=>{
+          card = document.querySelector('.flipper' + firstcard);
+          card.classList.toggle('is-flipped' + firstcard);
+          card = document.querySelector('.flipper' + seccard);
+          card.classList.toggle('is-flipped' + seccard);
+        },1500);
+
+        
+        setTimeout(()=>{
+          
+        },2000);
+
+        
+      }
+      numFlips = 0;
+    }
+    /*
     if(cardState === 0){
       setCardState(1);
-      card.classList.toggle('is-flipped');
+      card.classList.toggle('is-flipped'+fruit);
+      fruitPair[fruit] = 1;
+      console.log('The Flip Count is: ' + fruitPair[fruit]);
     }
     else {
       setCardState(0);
-      card.classList.toggle('is-flipped');
+      fruitPair[fruit] = 0;
+      card.classList.toggle('is-flipped'+fruit);
+      console.log('The Flip Count is: ' + fruitPair[fruit]);
     }
+    */
+    
   }
     return (
       <button onClick={handleFlip} class="buttonCol">
         <div class="flipcard">
-          <div class="flipper">
+          <div class={"flipper" + fruit}>
             <div class="front">
               <img src={front} alt="" height="150px" width="150px"></img>
             </div>
             <div class="back">
-              <img src={apple} alt="apple" height="150px" width="150px"></img>
+              <img src={cardType[fruit]} alt="apple" height="150px" width="150px"></img>
             </div>
           </div>
         </div>
